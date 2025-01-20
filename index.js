40e26b310af1ccb8405c0b40774cace31082d65d
Moshe Goldstein <mygguy613@gmail.com>
2025-01-20 16:49:08 -0500

    cl

--- index.js
+++ index.js
@@ -8,11 +8,18 @@ function submitData(name, email) {
         body: JSON.stringify({ name: name, email: email })
     })
         .then(res => { return res.json() })
-        // .then(json => console.log(json))
         .then(json => {
             const h1 = document.createElement('h1');
             h1.textContent = json.id;
             document.querySelector('body').appendChild(h1);
+        })
+        .catch(error => {
+            // alert('You\'ve got error!');
+            // console.log(error.message);
+            const h2 = document.createElement('h2');
+            h2.style.color = 'red';
+            h2.textContent = error.message;
+            document.querySelector('body').appendChild(h2);
         })
 }
 
